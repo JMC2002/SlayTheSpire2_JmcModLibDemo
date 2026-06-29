72575d34335cefc2a40679385d4089308f150ae9
MC J <jmc8@qq.com>
2026-06-29 17:40:06 +0800

    ci: add Steam Workshop publishing

---
+++ CHANGELOG_en.md
@@ -0,0 +1,7 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+## [1.1.0] - 2026-6-19
+### Changed
+- Adapted to the official mod publishing format migration.
--- Core/VersionInfo.cs
+++ Core/VersionInfo.cs
@@ -3,7 +3,7 @@ namespace JmcModLibDemo.Core;
 public static class VersionInfo
 {
     public const string Name = "JmcModLibDemo";
-    public const string Version = "1.0.9";
+    public const string Version = "1.1.0";
 
     public static string Tag => $"[{Name} v{Version}]";
 }
