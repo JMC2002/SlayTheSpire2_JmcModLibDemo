fc40659ae2dacca26909414af044e9918079508a
MC J <jmc8@qq.com>
2026-06-30 10:11:36 +0800

    chore: bump demo to 1.1.2

--- Core/VersionInfo.cs
+++ Core/VersionInfo.cs
@@ -3,7 +3,7 @@ namespace JmcModLibDemo.Core;
 public static class VersionInfo
 {
     public const string Name = "JmcModLibDemo";
-    public const string Version = "1.1.1";
+    public const string Version = "1.1.2";
 
     public static string Tag => $"[{Name} v{Version}]";
 }
